6c835a73dfcbd5d9f116e1be0cb9e687e76dc3f3
noxnoctis17 <petrsykora17@gmail.com>
2025-01-23 01:03:03 -0500

    begin removing trainer tables lol. also made blaze a pixilate ability

--- src/Tables/trainer_tables.c
+++ src/Tables/trainer_tables.c
@@ -25,38 +25,38 @@
 //     },
 // };
 
-const struct TrainerMonNoItemDefaultMoves sParty_RivalLabFire[] = {
-    {
-        .iv = 0,
-        .lvl = 5,
-        .species = SPECIES_TORCHIC,
-    },
-};
+// const struct TrainerMonNoItemDefaultMoves sParty_RivalLabFire[] = {
+//     {
+//         .iv = 0,
+//         .lvl = 5,
+//         .species = SPECIES_TORCHIC,
+//     },
+// };
 
-const struct TrainerMonNoItemDefaultMoves sParty_RivalLabGrass[] = {
-    {
-        .iv = 0,
-        .lvl = 5,
-        .species = SPECIES_TREECKO,
-    },
-};
+// const struct TrainerMonNoItemDefaultMoves sParty_RivalLabGrass[] = {
+//     {
+//         .iv = 0,
+//         .lvl = 5,
+//         .species = SPECIES_TREECKO,
+//     },
+// };
 
-const struct TrainerMonNoItemDefaultMoves sParty_RivalLabWater[] = {
-    {
-        .iv = 0,
-        .lvl = 5,
-        .species = SPECIES_MUDKIP,
-    },
-};
+// const struct TrainerMonNoItemDefaultMoves sParty_RivalLabWater[] = {
+//     {
+//         .iv = 0,
+//         .lvl = 5,
+//         .species = SPECIES_MUDKIP,
+//     },
+// };
 
-#define NO_NAME {_END, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE}
-#define RIVAL_NAME {_G, _a, _r, _y, _END}
+// #define NO_NAME {_END, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE, _SPACE}
+// #define RIVAL_NAME {_G, _a, _r, _y, _END}
 
 
-const struct Trainer gTrainers[] = {
-    [TRAINER_NONE] = {
-        .trainerName = NO_NAME,
-    },
+// const struct Trainer gTrainers[] = {
+//     [TRAINER_NONE] = {
+//         .trainerName = NO_NAME,
+//     },
 
     // [TRAINER_PICNICKER_MARTHA] = {
     //     .partyFlags = 0,
@@ -71,43 +71,43 @@ const struct Trainer gTrainers[] = {
     //     .party = { .NoItemDefaultMoves = sParty_PicMartha }
     // },
 
-    [TRAINER_RIVAL_OAKS_LAB_CHARMANDER] = {
-        .partyFlags = 0,
-        .trainerClass = CLASS_BEAUTY,
-        .encounterMusic = TRAINER_ENCOUNTER_MUSIC_FEMALE,
-        .trainerPic = TRAINER_PIC_BLUE,
-        .trainerName = RIVAL_NAME,
-        .items = {},
-        .doubleBattle = FALSE,
-        .aiFlags = AI_SCRIPT_CHECK_GOOD_MOVE,
-        .partySize = NELEMS(sParty_RivalLabFire),
-        .party = {.NoItemDefaultMoves = sParty_RivalLabFire}
-    },
+    // [TRAINER_RIVAL_OAKS_LAB_CHARMANDER] = {
+    //     .partyFlags = 0,
+    //     .trainerClass = CLASS_BEAUTY,
+    //     .encounterMusic = TRAINER_ENCOUNTER_MUSIC_FEMALE,
+    //     .trainerPic = TRAINER_PIC_BLUE,
+    //     .trainerName = RIVAL_NAME,
+    //     .items = {},
+    //     .doubleBattle = FALSE,
+    //     .aiFlags = AI_SCRIPT_CHECK_GOOD_MOVE,
+    //     .partySize = NELEMS(sParty_RivalLabFire),
+    //     .party = {.NoItemDefaultMoves = sParty_RivalLabFire}
+    // },
 
-    [TRAINER_RIVAL_OAKS_LAB_BULBASAUR] = {
-        .partyFlags = 0,
-        .trainerClass = CLASS_BEAUTY,
-        .encounterMusic = TRAINER_ENCOUNTER_MUSIC_FEMALE,
-        .trainerPic = TRAINER_PIC_BLUE,
-        .trainerName = RIVAL_NAME,
-        .items = {},
-        .doubleBattle = FALSE,
-        .aiFlags = AI_SCRIPT_CHECK_GOOD_MOVE,
-        .partySize = NELEMS(sParty_RivalLabGrass),
-        .party = {.NoItemDefaultMoves = sParty_RivalLabGrass}
-    },
+    // [TRAINER_RIVAL_OAKS_LAB_BULBASAUR] = {
+    //     .partyFlags = 0,
+    //     .trainerClass = CLASS_BEAUTY,
+    //     .encounterMusic = TRAINER_ENCOUNTER_MUSIC_FEMALE,
+    //     .trainerPic = TRAINER_PIC_BLUE,
+    //     .trainerName = RIVAL_NAME,
+    //     .items = {},
+    //     .doubleBattle = FALSE,
+    //     .aiFlags = AI_SCRIPT_CHECK_GOOD_MOVE,
+    //     .partySize = NELEMS(sParty_RivalLabGrass),
+    //     .party = {.NoItemDefaultMoves = sParty_RivalLabGrass}
+    // },
 
-    [TRAINER_RIVAL_OAKS_LAB_SQUIRTLE] = {
-        .partyFlags = 0,
-        .trainerClass = CLASS_BEAUTY,
-        .encounterMusic = TRAINER_ENCOUNTER_MUSIC_FEMALE,
-        .trainerPic = TRAINER_PIC_BLUE,
-        .trainerName = RIVAL_NAME,
-        .items = {},
-        .doubleBattle = FALSE,
-        .aiFlags = AI_SCRIPT_CHECK_GOOD_MOVE,
-        .partySize = NELEMS(sParty_RivalLabWater),
-        .party = {.NoItemDefaultMoves = sParty_RivalLabWater}
-    },
+    // [TRAINER_RIVAL_OAKS_LAB_SQUIRTLE] = {
+    //     .partyFlags = 0,
+    //     .trainerClass = CLASS_BEAUTY,
+    //     .encounterMusic = TRAINER_ENCOUNTER_MUSIC_FEMALE,
+    //     .trainerPic = TRAINER_PIC_BLUE,
+    //     .trainerName = RIVAL_NAME,
+    //     .items = {},
+    //     .doubleBattle = FALSE,
+    //     .aiFlags = AI_SCRIPT_CHECK_GOOD_MOVE,
+    //     .partySize = NELEMS(sParty_RivalLabWater),
+    //     .party = {.NoItemDefaultMoves = sParty_RivalLabWater}
+    // },
 
-};
+// };
